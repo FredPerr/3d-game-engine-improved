9b30f42c12ed3e669aa6907e5916a010ff9612b8
Frédéric Perron <frederic.norrep@gmail.com>
2019-01-10 22:50:04 -0500

    Add default files.

---
+++ src/main/java/ca/fredericperron/untitledgame/Main.java
@@ -0,0 +1,12 @@
+package ca.fredericperron.untitledgame;
+
+/**
+ * Created by Frédéric Perron on 2019-01-10. This file
+ * is under copyrights© as mentioned in the README file.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
